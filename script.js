324b749fd618ae6331e7220c258b994f8f05f198
Adrian Rymarz <adrian.rymarz1@gmail.com>
2024-03-18 19:55:21 +0100

    All direction elevations and new api

--- script.js
+++ script.js
@@ -91,6 +91,70 @@ function initializeMap()
     // Add marker to the map
     marker = L.marker([initialLatitude, initialLongitude], { draggable: true }).addTo(map);
 
+    var circle05km = L.circle([initialLatitude, initialLongitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.05,
+        weight: 1,
+        radius: 500 // promień w metrach (0.5 km = 500 m)
+    }).addTo(map);
+
+    var circle1km = L.circle([initialLatitude, initialLongitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.05,
+        weight: 1,
+        radius: 1000 // promień w metrach (1 km = 1000 m)
+    }).addTo(map);
+
+    var crossSize = 1100; // Wielkość krzyżyka w metrach (2 km = 2000 m)
+
+    // Linia pionowa
+    var verticalLine = L.polyline([
+        [initialLatitude - crossSize / 111000, initialLongitude], // Współrzędne punktu startowego
+        [initialLatitude + crossSize / 111000, initialLongitude]  // Współrzędne punktu końcowego
+    ], {
+        color: 'red',  // Kolor linii
+        weight: 1,      // Grubość linii w pikselach
+    }).addTo(map);
+
+    // Linia pozioma
+    var horizontalLine = L.polyline([
+        [initialLatitude, initialLongitude - crossSize / (111000 * Math.cos(initialLatitude * Math.PI / 180))], // Współrzędne punktu startowego
+        [initialLatitude, initialLongitude + crossSize / (111000 * Math.cos(initialLatitude * Math.PI / 180))]  // Współrzędne punktu końcowego
+    ], {
+        color: 'red',  // Kolor linii
+        weight: 1,      // Grubość linii w pikselach
+    }).addTo(map);
+
+    marker.on('move', function (event)
+    {
+        var markerPosition = event.latlng;
+
+        // Update circles position
+        circle05km.setLatLng(markerPosition);
+        circle1km.setLatLng(markerPosition);
+
+        var markerPosition = event.latlng;
+
+        // Oblicz nowe współrzędne linii
+        var newVerticalLineCoords = [
+            [markerPosition.lat - crossSize / 111000, markerPosition.lng],
+            [markerPosition.lat + crossSize / 111000, markerPosition.lng]
+        ];
+
+        var newHorizontalLineCoords = [
+            [markerPosition.lat, markerPosition.lng - crossSize / (111000 * Math.cos(markerPosition.lat * Math.PI / 180))],
+            [markerPosition.lat, markerPosition.lng + crossSize / (111000 * Math.cos(markerPosition.lat * Math.PI / 180))]
+        ];
+
+        // Ustaw nowe współrzędne dla linii
+        verticalLine.setLatLngs(newVerticalLineCoords);
+        horizontalLine.setLatLngs(newHorizontalLineCoords);
+
+        calculateAllDirectons()
+    });
+
     // Add event listener for marker dragend event
     marker.on('dragend', function (event)
     {
@@ -105,25 +169,61 @@ function initializeMap()
 
         latitudeElement.textContent = latitude;
         longitudeElement.textContent = longitude;
+
     });
 }
 
-// Funkcja do obliczania wysokości nad poziomem morza na podstawie współrzędnych geograficznych
-function calculateElevation(latitude, longitude)
+function calculateAllDirectons()
 {
-    var url = `https://api.open-elevation.com/api/v1/lookup?locations=${latitude},${longitude}`;
+    // Calculate elevation for directions
+    calculateElevationForDirection(latitude, longitude, 'north_05km', latitude + (0.5 / 111)); // 0.5 km north
+    calculateElevationForDirection(latitude, longitude, 'south_05km', latitude - (0.5 / 111)); // 0.5 km south
+    calculateElevationForDirection(latitude, longitude, 'east_05km', latitude, longitude + (0.5 / (111 * Math.cos(latitude * Math.PI / 180)))); // 0.5 km east
+    calculateElevationForDirection(latitude, longitude, 'west_05km', latitude, longitude - (0.5 / (111 * Math.cos(latitude * Math.PI / 180)))); // 0.5 km west
 
-    fetch(url)
+    calculateElevationForDirection(latitude, longitude, 'north_1km', latitude + (1 / 111)); // 1 km north
+    calculateElevationForDirection(latitude, longitude, 'south_1km', latitude - (1 / 111)); // 1 km south
+    calculateElevationForDirection(latitude, longitude, 'east_1km', latitude, longitude + (1 / (111 * Math.cos(latitude * Math.PI / 180)))); // 1 km east
+    calculateElevationForDirection(latitude, longitude, 'west_1km', latitude, longitude - (1 / (111 * Math.cos(latitude * Math.PI / 180)))); // 1 km west
+}
+
+// Funkcja do obliczania wysokości nad poziomem morza dla określonego kierunku z wykorzystaniem API Open-Meteo
+function calculateElevationForDirection(latitude, longitude, direction, newLatitude = latitude, newLongitude = longitude)
+{
+    const apiUrl = `https://api.open-meteo.com/v1/elevation?latitude=${newLatitude}&longitude=${newLongitude}`;
+
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data =>
         {
-            if (data && data.results && data.results.length > 0)
+            if (data.elevation && data.elevation.length > 0)
             {
-                var elevation = data.results[0].elevation;
+                const elevation = data.elevation[0];
+                document.getElementById(`elevation_${direction}`).textContent = `${elevation} meters`;
+            } else
+            {
+                document.getElementById(`elevation_${direction}`).textContent = 'Data not available';
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// Funkcja do obliczania wysokości nad poziomem morza na podstawie współrzędnych geograficznych z wykorzystaniem API Open-Meteo
+function calculateElevation(latitude, longitude)
+{
+    const apiUrl = `https://api.open-meteo.com/v1/elevation?latitude=${latitude}&longitude=${longitude}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data =>
+        {
+            if (data.elevation && data.elevation.length > 0)
+            {
+                const elevation = data.elevation[0];
                 document.getElementById('elevation').textContent = `${elevation} meters`;
             } else
             {
-                document.getElementById('elevation').textContent = ' data not available';
+                document.getElementById('elevation').textContent = 'Data not available';
             }
         })
         .catch(error => console.error('Error:', error));
